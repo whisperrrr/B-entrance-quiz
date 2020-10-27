28ea1c66b3ef115199d200b77ef3fc6be9369703
whisperrrr <jh731448520@163.com>
2020-10-27 21:14:41 +0800

    [Hui Jin][feat]-add crossorigin annotation

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/tier/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/tier/controller/StudentController.java
@@ -3,11 +3,13 @@ package com.thoughtworks.capability.gtb.entrancequiz.tier.controller;
 import com.thoughtworks.capability.gtb.entrancequiz.entity.Student;
 import com.thoughtworks.capability.gtb.entrancequiz.tier.service.StudentService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class StudentController {
     private final StudentService studentService;
